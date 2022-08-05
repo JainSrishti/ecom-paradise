4c0d7bd5c136ae8f7b0ca60d8a68557352c208aa
Jain Srishti <srishtijain1410@gmail.com>
2022-08-05 11:43:36 +0530

    Add base project to git

---
+++ src/main/java/com/app/ecomparadise/EcomParadiseApplication.java
@@ -0,0 +1,14 @@
+package com.app.ecomparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomParadiseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World with springboot after 2 years");
+		SpringApplication.run(EcomParadiseApplication.class, args);
+	}
+
+}
